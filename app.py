caf582c40a7a72842f131c0af5dcd883459ddce2
msuchiha26 <mario.silva@correounivalle.edu.co>
2026-04-27 22:57:29 -0500

    Reorder parameters in get_connection function

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def get_connection():
     return psycopg2.connect(
         host=os.environ.get("DB_HOST"),
         database=os.environ.get("DB_NAME"),
-        user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
-        port=os.environ.get("DB_PORT")
+        port=os.environ.get("DB_PORT"),
+        user=os.environ.get("DB_USER")        
     )
 
 @app.route('/')
